cb0ece307867156a5532d3b0955ffcc1a25e3454
Mikhil Desai <mikhildesai@gmail.com>
2020-05-20 22:57:05 +0530

    Calculating Monthly Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
 wage_per_hour=20
+working_days=20
 full_time=8
 part_time=4
 
@@ -9,11 +10,11 @@ empcheck=$[(RANDOM%3)]
 case $empcheck in
  
 		1 )
-			wage=$[($wage_per_hour*$full_time)]
+			wage=$[($wage_per_hour*$full_time*$working_days)]
 			;;
 
 		2 )
-			wage=$[($wage_per_hour*$part_time)]
+			wage=$[($wage_per_hour*$part_time*$working_days)]
 			;;
 		* )
 			wage=0

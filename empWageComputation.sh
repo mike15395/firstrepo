ce35a3e8d6f6c57ee2d39d4dee928724d159716e
Mikhil Desai <mikhildesai@gmail.com>
2020-05-20 23:24:38 +0530

    while loop condition added and made major changes

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,23 +1,32 @@
 #!/bin/bash -x
 
 wage_per_hour=20
-working_days=20
-full_time=8
-part_time=4
+max_monthly_hours=100
+num_working_days=20
+is_full_time=1
+is_part_time=2
+
+total_working_days=0
+total_emp_hrs=0
 
 empcheck=$[(RANDOM%3)]
-
+while [[ $total_working_days -lt $num_working_days && $total_emp_hrs -lt $max_monthly_hours ]]
+do
+((total_working_days++))
 case $empcheck in
  
-		1 )
-			wage=$[($wage_per_hour*$full_time*$working_days)]
-			;;
+		$is_full_time )
+				emphrs=8
+				;;
 
-		2 )
-			wage=$[($wage_per_hour*$part_time*$working_days)]
-			;;
+		$is_part_time )
+				emphrs=4
+				;;
 		* )
-			wage=0
-			;;
+				emphrs=0
+				;;
 esac
-
+		total_emp_hrs=$[(total_emp_hrs+emphrs)]
+		dailywage=$[(emphrs*wage_per_hour)]
+		monthlywage=$[(dailywage*num_working_days)]
+done

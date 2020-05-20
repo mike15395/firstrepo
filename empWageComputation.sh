8708861c5fb796bcf8a9411601fe5ee32bfc3fce
Mikhil Desai <mikhildesai@gmail.com>
2020-05-20 22:45:32 +0530

    added partime employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,13 +2,21 @@
 
 wage_per_hour=20
 full_time=8
+part_time=4
 
-empcheck=$[(RANDOM%2)]
+empcheck=$[(RANDOM%3)]
 
-if (( $empcheck ==1 ))
-then
-	wage=$[($wage_per_hour*$full_time)]
-else
-	wage=0
-fi
+case $empcheck in
+ 
+		1 )
+			wage=$[($wage_per_hour*$full_time)]
+			;;
+
+		2 )
+			wage=$[($wage_per_hour*$part_time)]
+			;;
+		* )
+			wage=0;
+			;;
+esac
 
